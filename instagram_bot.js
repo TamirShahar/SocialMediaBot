fce30308fabbbb913aa0347f50c791cf7bba432b
44dave <122266242+44dave@users.noreply.github.com>
2023-10-30 17:27:43 +0200

    Posting and changing language implemented for IG
    posting problems - when posting a reel, the video takes too long to load and we et a timeout error.
    Language change - delay between button clicks should be improved

--- instagram_bot.js
+++ instagram_bot.js
@@ -11,11 +11,13 @@ class InstagramBot extends SocialMediaBot {
   async log_in(username, password) {
     try{
       await this.page.goto('https://www.instagram.com/?lang=en');
-      await this.page.getByLabel('Phone number, username, or email').click();
-      await this.page.getByLabel('Phone number, username, or email').fill(username);
+      
+      await this.page.getByLabel("Phone number, username, or email").click();
+      await this.page.getByLabel("Phone number, username, or email").fill(username);
       await this.page.getByLabel('Password').click();
       await this.page.getByLabel('Password').fill(password);
       await this.page.getByRole('button', { name: 'Log in', exact: true }).click();    
+      await delay(1000);
       await this.page.goto('https://www.instagram.com/?lang=en');
       const continueButton = await this.waitForElement('button:has-text("not now")');
       if (continueButton) {
@@ -76,6 +78,143 @@ class InstagramBot extends SocialMediaBot {
   async send_dm(username, msg_str) {
     // Implement send_dm logic for Instagram
   }
+
+  // TODO: methods upload_post, upload_story, and upload_reel aren't implementations of SocialMediaBot's abstract methods.
+  // TODO: These methods also have not been properly tested.
+  async upload_post(inputFiles, captions, location=NaN) {
+    // UNTESTED CODE:
+    try {
+      // Click on create new post button
+      await this.page.getByRole('link', { name: 'New post Create' }).click();
+      await delay(2000);
+      // Select the file from computer
+      const fileInput = await this.page.$('input[accept="image/jpeg,image/png,image/heic,image/heif,video/mp4,video/quicktime"][type="file"]');
+
+      //const send_files_element = await this.page.getByRole('button', { name: 'Select From Computer' });
+      //await this.page.getByRole('button', { name: 'Select From Computer' }).click();
+      await fileInput.setInputFiles(inputFiles);
+
+      // Hit "Next" twice
+      await this.page.locator('div').filter({ hasText: /^Next$/ }).nth(1).click();
+      await this.page.getByRole('button', { name: 'Next' }).click();
+
+      // Add captions
+      if (captions) {
+        await this.page.getByRole('paragraph').click();
+        // await this.page.getByLabel('Write a caption...').click();
+        await this.page.getByLabel('Write a caption...').fill(captions);
+      }
+
+      // Optionally, add location
+      if (location) {
+        await this.page.getByPlaceholder('Add Location').click();
+        await this.page.getByPlaceholder('Add Location').fill(location);
+        await this.page.getByRole('button', { name: location, exact: true }).first().click();
+      }
+      
+      // Finally, share.
+      await this.page.getByRole('button', { name: 'Share' }).click();
+      await delay(3000);
+      await this.page.locator('div').filter({ hasText: /^Your post has been shared\.$/ }); // Make sure the success indicator exists
+      await this.page.getByRole('button', { name: 'Close' }).click();      
+
+    } catch (error) {
+      console.error(error);
+      return false; // Return false in case of any errors
+    }
+  }
+
+  async upload_story(inputFile) {
+    // UNTESTED CODE:
+    try {
+      // TODO: Here, add a line of code to convert the viewport to mobile.
+      // There's no option to add a story on PC view, but you can trick Instagram by shifting to mobile screen aspect ratio.
+      try {
+        await this.page.getByRole('button', { name: 'Not now' }).click();
+      } catch (error) {
+        await this.page.reload();
+      }
+      await this.page.getByRole('button', { name: 'Story Story' }).setInputFiles(inputFile);
+    } catch (error) {
+      console.error(error);
+      return false; // Return false in case of any errors
+    }
+  }
+
+  async upload_reel(inputFiles, captions, location) {
+    // UNTESTED CODE:
+    // Instagram prompts you that "video posts are now shared as reels".
+    // This is expressed in the code: it's almost identical to upload_post.
+    try {
+      // Click on create new post button
+      await this.page.getByRole('link', { name: 'New post Create' }).click();
+      await delay(2000);
+      // Select the file from computer
+      const fileInput = await this.page.$('input[accept="image/jpeg,image/png,image/heic,image/heif,video/mp4,video/quicktime"][type="file"]');
+
+      //const send_files_element = await this.page.getByRole('button', { name: 'Select From Computer' });
+      //await this.page.getByRole('button', { name: 'Select From Computer' }).click();
+      await fileInput.setInputFiles(inputFiles);
+      await delay(5000);
+      // Hit "Next" twice
+      await this.page.locator('div').filter({ hasText: /^Next$/ }).nth(1).click();
+      await this.page.getByRole('button', { name: 'Next' }).click();
+
+      // Add captions
+      if (captions) {
+        await this.page.getByRole('paragraph').click();
+        // await this.page.getByLabel('Write a caption...').click();
+        await this.page.getByLabel('Write a caption...').fill(captions);
+      }
+
+      // Optionally, add location
+      if (location) {
+        await this.page.getByPlaceholder('Add Location').click();
+        await this.page.getByPlaceholder('Add Location').fill(location);
+        await this.page.getByRole('button', { name: location, exact: true }).first().click();
+      }
+      
+      // Finally, share.
+      await this.page.getByRole('button', { name: 'Share' }).click();
+      await delay(3000);
+      await this.page.locator('div').filter({ hasText: /^Your reel has been shared\.$/ }); // Make sure the success indicator exists
+      await this.page.getByRole('button', { name: 'Close' }).click();      
+    } catch (error) {
+      console.error(error);
+      return false; // Return false in case of any errors
+    }
+  }
+
+  async change_language_to_english()
+  {
+    await this.page.goto('https://instagram.com/?lang=en');
+    await delay(1000);
+    const element = await this.page.$('//html/body/div[2]/div/div/div[2]/div/div/div/div[1]/div[1]/div[1]/div/div/div/div/div[2]/div[8]/div');
+    await element.click();
+    ///html/body/div[2]/div/div/div[2]/div/div/div/div[1]/div[1]/div[2]/div[2]/section/main/div/header/section/div[1]/div[2]/div
+    await delay(2000);
+  //  await this.page.goto()
+//    await this.page.locator('div:nth-child(8) > div > .x4k7w5x > div').click();
+    const optionsButton = await this.page.$('//html/body/div[2]/div/div/div[2]/div/div/div/div[1]/div[1]/div[2]/div[2]/section/main/div/header/section/div[1]/div[2]/div');
+    await optionsButton.click();
+//    await this.page.getByRole('button', { name: 'Settings and privacy' }).click();
+    await delay(2000);
+    const privacyButton = await this.page.$('//html/body/div[5]/div[1]/div/div[2]/div/div/div/div/div[2]/div/div/div/button[4]');
+    await privacyButton.click();
+    await delay(1000);
+    const languageButton = await this.page.$('//html/body/div[2]/div/div/div[2]/div/div/div/div[1]/div[1]/div[2]/section/main/div/div[1]/div/div/div/div/div[4]/a');
+    await languageButton.click();
+   // await this.page.getByRole('link', { name: 'Language preferences' }).click();
+   await delay(1000);
+   const searchButton = await this.page.$('//html/body/div[2]/div/div/div[2]/div/div/div/div[1]/div[1]/div[2]/section/main/div/div[3]/div/div/div[2]/input');
+   await searchButton.click();
+   //    await this.page.getByPlaceholder('Search').click();
+    await searchButton.fill('English');
+    //await this.page.getByRole('button', { name: 'English Checkmark filled icon' }).click();
+    const englishButton = await this.page.$('//html/body/div[2]/div/div/div[2]/div/div/div/div[1]/div[1]/div[2]/section/main/div/div[3]/div/div/div[2]/div[2]/div[1]');
+   await englishButton.click();
+  }
+
 }
 
 
